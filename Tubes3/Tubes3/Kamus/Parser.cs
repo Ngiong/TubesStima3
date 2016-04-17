20db14afc457ef47b625bf9391c28f71bab1c262
Ali Akbar <aliakbr@outlook.co.id>
2016-04-17 16:05:27 +0700

    Kelas baru untuk parsing argumen ke twitter API

---
+++ Tubes3/Tubes3/Kamus/Parser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tubes3.Kamus
+{
+    public class Parser
+    {
+        public string Parse(string input)
+        {
+            string result = "";
+            int j = 0;
+            while (j < input.Length)
+            {
+                if (input[j] == ' ')
+                {
+                    result += "%20";
+                }
+                else if (input[j] == '@')
+                {
+                    result += "%40";
+                }
+                else if (input[j] == '#')
+                {
+                    result += "%23";
+                }
+                else if (input[j] == ';')
+                {
+                    result += "%20OR%20";
+                }
+                else
+                {
+                    result += input[j];
+                }
+                j++;
+            }
+            return result;
+        }
+    }
+}
