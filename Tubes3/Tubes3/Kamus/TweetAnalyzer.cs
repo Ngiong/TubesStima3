ec722c190a6cc54a0ffe76725e32f8d95b39f21f
Ngiong <ngiooong@yahoo.com>
2016-04-24 18:33:49 +0700

    Tambahin komentar buat TweetAnalyzer

--- Tubes3/Tubes3/Kamus/TweetAnalyzer.cs
+++ Tubes3/Tubes3/Kamus/TweetAnalyzer.cs
@@ -7,15 +7,18 @@ namespace Tubes3.Kamus
 {
     public class TweetAnalyzer
     {
-        private Tweet target;
-        private Dictionary dictionary;
+        private Tweet target; // Reference ke Tweet yang akan di analisis
+        private Dictionary dictionary; // Reference ke Dictionary yang dipakai
         private int type; // type 0 : KMP, type 1 : BM
 
+        // Constructor
         public TweetAnalyzer(Tweet t, Dictionary d, int tp)
         {
             target = t; dictionary = d; type = tp;
         }
 
+        // Melakukan analisis sebuah Tweet dengan sebuah dictionary, dan mengembalikan
+        // indeks terkecil munculnya sebuah keyword dari Dictionary yang diacu.
         public int analyzeTweet()
         {
             List<List<string>> dict = dictionary.getDictionary();
